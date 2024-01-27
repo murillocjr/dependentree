2d687a723291fe56ad3490c2a3921b23e9e4101e
netmaster <murillo.cjr@gmail.com>
2024-01-27 06:52:20 +0000

    working for me

--- data/index.js
+++ data/index.js
@@ -1,6 +1,9 @@
+const html2canvas = require('html2canvas');
+
 export default {
   BinaryTreeMaker: require('./binary'),
   LinkedListMaker: require('./linked-list'),
   rps: require('./rock-paper-scissors'),
   royals: require('./royals'),
+  html2canvas
 }
